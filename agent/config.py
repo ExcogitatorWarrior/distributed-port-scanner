2a91475cd2fc78a58eee9d173f792f2e1c1b2e1d
ExcogitatorWarrior <ohotniknapkb@mail.ru>
2026-04-23 20:50:04 +0300

    minor change

--- agent/config.py
+++ agent/config.py
@@ -1,5 +1,5 @@
 #agent/config.py
 BASE_URL = "http://127.0.0.1:8000"
 SECRET = "13e3e5a7c895a56cbfa080b25775c822ccde7a83b9b79d8d779d85ffe85feaa1"
-CONTACT_INTERVAL_DEFAULT = 3-  # Default interval in seconds (1 hour)
+CONTACT_INTERVAL_DEFAULT = 30  # Default interval in seconds (1 hour)
 TASK_DB_PATH = "tasks_db.json"  # Path to local task storage
